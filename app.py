9efdead1c8a9c1977ed46b59f3afbc7a7fa75c82
Charles Morin <charles.mor@outlook.fr>
2019-06-24 14:14:22 -0400

    Work on download

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import youtube_dl
 
 app = Flask(__name__)
+
+MP3_DIRECTORY = './MP3' # must be in config.py
 
 @app.route('/')
 def home():
@@ -23,7 +25,7 @@ def convert():
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         url = request.form['url']
         info_dict = ydl.extract_info(url)
-        fn = ydl.prepare_filename(info_dict).replace('webm', 'mp3')
+        fn = ydl.prepare_filename(info_dict).replace('webm', 'mp3')[6:]
         ydl.download([url])
     return render_template('done.html', download_path=fn)
 
@@ -31,8 +33,9 @@ def convert():
 def done():
     return render_template('done.html')
 
-@app.route('/download')
-def download():
+@app.route('/download/<download_path>')
+def download(download_path = None):
+    send_from_directory(MP3_DIRECTORY, download_path, as_attachment=True)
     return render_template('done.html')
 
 @app.errorhandler(404)
